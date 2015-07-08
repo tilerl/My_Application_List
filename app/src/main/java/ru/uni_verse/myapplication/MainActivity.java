1f93e099602a22fa40e4b32277e3f97c8dd39450
TilerL <tilerlip@gmail.com>
2015-07-09 00:24:39 +0300

    My first commit

---
+++ app/src/main/java/ru/uni_verse/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package ru.uni_verse.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Person> mPeople;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView rv = (RecyclerView)findViewById(R.id.rv);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+
+        mPeople = new ArrayList<>();
+        mPeople.add(new Person("Adam Allen", "23", R.drawable.adam));
+        mPeople.add(new Person("Brian Benjamen", "44", R.drawable.brian));
+        mPeople.add(new Person("Charles Clive", "32", R.drawable.charles));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/ru/uni_verse/myapplication/Person.java
@@ -0,0 +1,16 @@
+package ru.uni_verse.myapplication;
+
+/**
+ * Created by Feder2 on 08.07.2015.
+ */
+public class Person {
+    String name;
+    String age;
+    int photoId;
+
+    Person(String name, String age, int photoId){
+        this.name = name;
+        this.age = age;
+        this.photoId = photoId;
+    }
+}
