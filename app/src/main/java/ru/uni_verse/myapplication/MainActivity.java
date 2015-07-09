ad8432cca5dfadf08ac8ff57bdd5293f7b6ff4e9
Tiler L <tilerlip@gmail.com>
2015-07-09 13:44:29 +0300

    Implemente CardView and RecylerView

--- app/src/main/java/ru/uni_verse/myapplication/MainActivity.java
+++ app/src/main/java/ru/uni_verse/myapplication/MainActivity.java
@@ -3,8 +3,10 @@ package ru.uni_verse.myapplication;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -28,6 +30,24 @@ public class MainActivity extends AppCompatActivity {
         mPeople.add(new Person("Adam Allen", "23", R.drawable.adam));
         mPeople.add(new Person("Brian Benjamen", "44", R.drawable.brian));
         mPeople.add(new Person("Charles Clive", "32", R.drawable.charles));
+        mPeople.add(new Person("Dennis David", "42", R.drawable.adam));
+        mPeople.add(new Person("Elijah Ellison", "19", R.drawable.brian));
+        mPeople.add(new Person("Frederik Franklin", "26", R.drawable.charles));
+        mPeople.add(new Person("Geralt Gerry", "40", R.drawable.adam));
+        mPeople.add(new Person("Henry Hudson", "39", R.drawable.brian));
+        mPeople.add(new Person("Ivan Idaho", "32", R.drawable.charles));
+        mPeople.add(new Person("Adam Allen", "23", R.drawable.adam));
+        mPeople.add(new Person("Brian Benjamen", "44", R.drawable.brian));
+        mPeople.add(new Person("Charles Clive", "32", R.drawable.charles));
+        mPeople.add(new Person("Dennis David", "42", R.drawable.adam));
+        mPeople.add(new Person("Elijah Ellison", "19", R.drawable.brian));
+        mPeople.add(new Person("Frederik Franklin", "26", R.drawable.charles));
+        mPeople.add(new Person("Geralt Gerry", "40", R.drawable.adam));
+        mPeople.add(new Person("Henry Hudson", "39", R.drawable.brian));
+        mPeople.add(new Person("Ivan Idaho", "32", R.drawable.charles));
+
+        RVAdapter adapter = new RVAdapter(mPeople);
+        rv.setAdapter(adapter);
     }
 
     @Override
--- app/src/main/java/ru/uni_verse/myapplication/RVAdapter.java
+++ app/src/main/java/ru/uni_verse/myapplication/RVAdapter.java
@@ -1,7 +1,63 @@
 package ru.uni_verse.myapplication;
 
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by Feder2 on 09.07.2015.
  */
-public class RVAdapter {
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> {
+
+    List<Person> people;
+
+    RVAdapter(List<Person> people){
+        this.people = people;
+    }
+
+    @Override
+    public PersonViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view, parent, false);
+        PersonViewHolder personViewHolder = new PersonViewHolder(v);
+        return personViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(PersonViewHolder holder, int position) {
+        holder.personName.setText(people.get(position).name);
+        holder.personAge.setText(people.get(position).age);
+        holder.personPhoto.setImageResource(people.get(position).photoId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return people.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView){
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public static class PersonViewHolder extends RecyclerView.ViewHolder{
+
+        CardView cardView;
+        TextView personName;
+        TextView personAge;
+        ImageView personPhoto;
+
+        PersonViewHolder(View itemView){
+            super(itemView);
+            cardView = (CardView)itemView.findViewById(R.id.cv);
+            personName = (TextView)itemView.findViewById(R.id.personName);
+            personAge = (TextView)itemView.findViewById(R.id.personAge);
+            personPhoto = (ImageView)itemView.findViewById(R.id.personPhoto);
+        }
+    }
 }
